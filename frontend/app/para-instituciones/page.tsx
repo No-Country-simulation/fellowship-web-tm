8893aa41d59a0cde06c0b3b8cb97e8a234e6adfa
lorenzosegada <lorenzosegada@gmail.com>
2026-09-08 17:08:05 -0300

    feat: se agrega CasoOracle

--- frontend/app/para-instituciones/page.tsx
+++ frontend/app/para-instituciones/page.tsx
@@ -1,11 +1,13 @@
 import TalentPipeline from "@/components/organisms/para-instituciones/TalentPipeline";
 import Diferenciacion from "@/components/organisms/para-instituciones/Diferenciacion";
+import CasoDeExito from "@/components/organisms/para-instituciones/CasoDeExito";
 
 export default function Page() {
   return (
     <main>
       <TalentPipeline />
       <Diferenciacion />
+      <CasoDeExito />
     </main>
   );
 }
