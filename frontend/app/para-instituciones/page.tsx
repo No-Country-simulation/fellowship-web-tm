e311856082896f88d1a8851105458008cbb5c741
lorenzosegada <lorenzosegada@gmail.com>
2026-09-08 16:36:30 -0300

    feat: se agrega diferenciacion

--- frontend/app/para-instituciones/page.tsx
+++ frontend/app/para-instituciones/page.tsx
@@ -1,9 +1,11 @@
 import TalentPipeline from "@/components/organisms/para-instituciones/TalentPipeline";
+import Diferenciacion from "@/components/organisms/para-instituciones/Diferenciacion";
 
 export default function Page() {
   return (
     <main>
       <TalentPipeline />
+      <Diferenciacion />
     </main>
   );
 }
