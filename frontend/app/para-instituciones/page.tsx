117f1635816186528af663cedeb321a0331eb020
lorenzosegada <lorenzosegada@gmail.com>
2026-09-08 17:20:08 -0300

    feat: se agrega adatable.tsx

--- frontend/app/para-instituciones/page.tsx
+++ frontend/app/para-instituciones/page.tsx
@@ -1,6 +1,7 @@
 import TalentPipeline from "@/components/organisms/para-instituciones/TalentPipeline";
 import Diferenciacion from "@/components/organisms/para-instituciones/Diferenciacion";
 import CasoDeExito from "@/components/organisms/para-instituciones/CasoDeExito";
+import Adaptable from "@/components/organisms/para-instituciones/Adaptable";
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <TalentPipeline />
       <Diferenciacion />
       <CasoDeExito />
+      <Adaptable />
     </main>
   );
 }
